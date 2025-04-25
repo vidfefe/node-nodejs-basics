545b5c955b69a5d41e7f7bb518ce984379855be4
vidfefe <malinovskaaviktoria7@gmail.com>
2025-04-25 09:54:15 +0300

    feat: complete list.js task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,15 @@
+import path from 'path';
+import fs from 'fs/promises';
+
 const list = async () => {
-    // Write your code here 
+    const dirPath = path.join('src', 'fs', 'files');
+
+    try {
+        const files = await fs.readdir(dirPath);
+        console.log(files);
+    } catch (err) {
+       throw new Error('FS operation failed');
+    }
 };
 
 await list();
