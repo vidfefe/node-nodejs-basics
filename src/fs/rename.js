2fa787ce65721050ae58738ad04221be4d8827d5
vidfefe <malinovskaaviktoria7@gmail.com>
2025-04-25 09:08:45 +0300

    feat: complete rename.js task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,27 @@
+import path from 'path';
+import fs from 'fs/promises'
+
 const rename = async () => {
-    // Write your code here 
+    const oldPath = path.join('src', 'fs', 'files', 'wrongFilename.txt');
+    const newPath = path.join('src', 'fs', 'files', 'properFilename.md');
+
+    try{
+        await fs.access(oldPath);
+
+        try{
+            await fs.access(newPath);
+            throw new Error('FS operation failed');
+        } catch (err) {
+            if( err.code === 'ENOENT'){
+                await fs.rename(oldPath, newPath);
+            } else {
+                throw new Error('FS operation failed');
+            }
+        }
+
+    } catch(err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await rename();
