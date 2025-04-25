c510c2c3ac88d9a873d6f4533e12e874518951f9
vidfefe <malinovskaaviktoria7@gmail.com>
2025-04-25 13:29:04 +0300

    feat: complete hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+import fs from 'fs';
+import { createHash } from 'crypto';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const hash = createHash('SHA256');
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+    const fileStream = fs.createReadStream(filePath);
+    fileStream.on('data', chunk => {
+        console.log(hash.update(chunk).digest('hex'));
+    });
 };
 
 await calculateHash();
