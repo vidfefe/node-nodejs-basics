299102774e59de0d857ea3f7f9b9e319d2658144
vidfefe <malinovskaaviktoria7@gmail.com>
2025-04-25 08:51:17 +0300

    feat: complete copy.js task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,22 @@
+import path from 'path';
+import fs from 'fs/promises'
+
 const copy = async () => {
-    // Write your code here 
+    const sourcePath = path.join('src', 'fs', 'files');
+    const destPath = path.join('src', 'fs', 'files_copy');
+
+    try {
+        const sourceStat = await fs.stat(sourcePath);
+        const destExists = await fs.access(destPath).then(() => true).catch(() => false);
+
+        if (!sourceStat.isDirectory() || destExists) {
+            throw new Error();
+        }
+
+        await fs.cp(sourcePath, destPath, { recursive: true });
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await copy();
