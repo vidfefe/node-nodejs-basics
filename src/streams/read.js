ebbc5535afd220c08eca6615365888d3d6924371
vidfefe <malinovskaaviktoria7@gmail.com>
2025-04-26 14:16:24 +0300

    feat: complete streams tasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
+    const readStream = fs.createReadStream(filePath, { encoding: 'utf-8' });
+    readStream.pipe(process.stdout);
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+   const filePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+
+   const writeStream = fs.createWriteStream(filePath);
+   process.stdin.pipe(writeStream);
 };
 
 await write();
